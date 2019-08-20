01c72d07232568f83f41d0761efbf102fee44813
Pieter van Goor <pieter.vangoor@anu.edu.au>
2019-08-20 14:42:25 +1000

    Add script to convert event cam bags to file data

---
+++ rosbag/events_bag2file.py
@@ -0,0 +1,63 @@
+import sys, getopt, os
+import rosbag
+
+import cv2
+import cv_bridge
+import progressbar
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"b:")
+except getopt.GetoptError:
+    print 'events_bag2file.py -b <bagfile>'
+    sys.exit(2)
+
+bagname = 'calib_texture_50.bag'
+
+bag = rosbag.Bag(bagname, 'r')
+messageNumber = bag.get_message_count()
+
+
+bridge = cv_bridge.CvBridge()
+
+# Prepare output directory
+assert(bagname.endswith(".bag"))
+outputdir = bagname[:-4]
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
+
+imagetimefname = outputdir+"/"+"images.txt"
+imagetimefile = open(imagetimefname, 'w')
+eventFname = outputdir+"/"+"events.txt"
+eventFile = open(eventFname, 'w')
+imagedir = outputdir+"/"+"images"
+if not os.path.exists(imagedir):
+    os.makedirs(imagedir)
+
+imageCounter = 0
+msgCounter = 0
+progBar = progressbar.ProgressBar(max_value=messageNumber)
+for topic, msg, t in bag.read_messages():
+    if topic == "/dvs/image_raw":
+        img = bridge.imgmsg_to_cv2(msg)
+        imageCounter += 1
+        imagefilename = "images/{:08d}.png".format(imageCounter)
+        timestamp = "{:d}.{:09d}".format(msg.header.stamp.secs,msg.header.stamp.nsecs)
+        
+        cv2.imwrite(outputdir+"/"+imagefilename, img)
+
+        line = timestamp + " " + imagefilename + "\n"
+        imagetimefile.writelines(line)
+
+    elif topic == "/dvs/events":
+        for emsg in msg.events:
+            timestamp = "{:d}.{:09d}".format(emsg.ts.secs,emsg.ts.nsecs)
+            eline = "{} {:d} {:d} {:d}\n".format(timestamp, emsg.x, emsg.y, emsg.polarity)
+            eventFile.writelines(eline)
+
+    else:
+        print topic
+    
+    msgCounter += 1
+    progBar.update(msgCounter)
+
+progBar.update(messageNumber)
