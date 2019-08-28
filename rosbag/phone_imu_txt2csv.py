228a4ffc5d64a44b5ff5e540e551c254863a4acf
Pieter van Goor <pieter.vangoor@anu.edu.au>
2019-08-29 09:43:31 +1000

    Add phone IMU and video converter

---
+++ rosbag/phone_imu_txt2csv.py
@@ -0,0 +1,103 @@
+'''
+Phone Sensor Data Converter: txt to csv
+Usage: phone_imu_txt2csv.py -f"
+Arguments:
+ -f, --file         The file containing IMU data in the sensor log format.
+
+Flags:
+ -h                 help
+'''     
+
+import csv
+import numpy as np
+import progressbar
+
+import getopt
+import sys
+
+def usage():
+    print sys.exit(__doc__)
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def write_csv_msg(writer, msg_dict, timestamp):
+    record_list = ['GYR', 'ACC', 'MAG']
+    row = [timestamp]
+    for record in record_list:
+        if record in msg_dict:
+            row += msg_dict[record]
+        else:
+            break
+    else:
+        writer.writerow(row)
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "f:", ["file="])
+except getopt.GetoptError as err:
+    print(str(err))
+    usage()
+
+for opt, arg in opts:
+    if opt == '-h':
+        usage()
+
+    elif opt in ("-f", "--file"):
+        imu_fname_txt = arg
+        if not imu_fname_txt.endswith(".txt"):
+            imu_fname_txt = imu_fname_txt + ".txt"
+        
+        try:
+            imu_file_txt = open(imu_fname_txt, 'r')
+        except IOError:
+            print "Could not find the IMU text file."
+            sys.exit(1)
+
+progBar = progressbar.ProgressBar(max_value=file_len(imu_fname_txt))
+
+imu_fname_csv = imu_fname_txt[:-4] + ".csv"
+imu_file_csv = open(imu_fname_csv, 'w')
+imu_csv_writer = csv.writer(imu_file_csv)
+
+header_row = ["TIME", "GYRX", "GYRY", "GYRZ", "ACCX", "ACCY", "ACCZ", "MAGX", "MAGY", "MAGZ"]
+imu_csv_writer.writerow(header_row)
+
+
+# Read through the file
+current_time = -1
+msg_dict = {}
+line_count = 0
+for txt_row in imu_file_txt:
+    txt_row = txt_row.strip('\n')
+    txt_row = txt_row.split("\t")
+
+    timestamp = int(txt_row[0]) * 1e-3
+    if (current_time < 0):
+        current_time = timestamp
+    
+    msg_type = txt_row[1]
+    msg_val = [float(val) for val in txt_row[2:]]
+    
+    if abs(timestamp - current_time) < 1e-6:
+        # Collect messages with the same timestamps
+        msg_dict[msg_type] = msg_val
+    
+    else:
+        # Write when a new timestamp appears
+        write_csv_msg(imu_csv_writer, msg_dict, timestamp)
+        current_time = timestamp
+        msg_dict = {msg_type:msg_val}
+
+    line_count += 1
+    progBar.update(line_count)
+
+
+
+
+
+imu_file_txt.close()
+imu_file_csv.close()
---
+++ rosbag/video_imu2rosbag.py
@@ -0,0 +1,150 @@
+'''
+Video and IMU to ROSbag Converter
+Usage: video_imu2rosbag.py -i <imu> -v <video> -t <time"
+Arguments:
+ -i, --imu          The file containing timestamped IMU data in csv format.
+ -v, --video        The video file to be used.
+ -t, --time         The starting time of the video w.r.t the IMU data in seconds. Default: 0
+ -b, --bag          The output bag file name. Default: output.bag
+ -s, --scale        The scale of the video output. e.g. 0.5 will halve the resolution. Default: 1
+
+Flags:
+ -h                 help
+'''     
+
+import cv2
+import rosbag
+import rospy
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import Imu
+import cv_bridge
+import csv
+import numpy as np
+import progressbar
+
+import getopt
+import sys
+
+def usage():
+    print sys.exit(__doc__)
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "i:v:tbs", ["imu=", "video=", "time=", "bag=", "scale="])
+except getopt.GetoptError as err:
+    print(str(err))
+    usage()
+
+bag_fname = "output.bag"
+scale = 0.5
+time_offset = 0
+
+for opt, arg in opts:
+    if opt == '-h':
+        usage()
+
+    elif opt in ("-i", "--imu"):
+        imu_fname = arg
+
+    elif opt in ("-v", "--video"):
+        video_fname = arg
+    
+    elif opt in ("-t", "--time"):
+        try:
+            time_offset = float(arg)
+        except ValueError:
+            print("Time must be provided as a number.")
+            sys.exit(1)
+    
+    elif opt in ("-s", "--scale"):
+        try:
+            scale = float(arg)
+        except ValueError:
+            print("Scale must be provided as a number.")
+            print(opts)
+            sys.exit(1)
+    
+    elif opt in ("-b", "--bag"):
+        bag_fname = arg
+        if not bag_fname.endswith(".bag"):
+            bag_fname = bag_fname + ".bag"
+
+
+# Open the bag file
+bag_file = open(bag_fname, 'w')
+bag = rosbag.Bag(bag_file,'w')
+
+
+# Convert the IMU data
+imu_file_length = file_len(imu_fname)
+imu_file = open(imu_fname, 'r')
+imu_reader = csv.reader(imu_file)
+
+print("Processing IMU...")
+progBar = progressbar.ProgressBar(max_value=imu_file_length)
+count = 0
+for row in imu_reader:
+    # Skip the header
+    if count == 0:
+        count += 1
+        continue
+
+    timestamp = float(row[0])
+    gyr_xyz = [float(a) for a in row[1:4]]
+    acc_xyz = [float(a) for a in row[4:7]]
+
+    # print gxyz
+    # print axyz
+
+    rosimu = Imu()
+    rosimu.header.stamp = rospy.Time(timestamp)
+    rosimu.angular_velocity.x = gyr_xyz[0]
+    rosimu.angular_velocity.y = gyr_xyz[1]
+    rosimu.angular_velocity.z = gyr_xyz[2]
+    rosimu.linear_acceleration.x = acc_xyz[0]
+    rosimu.linear_acceleration.y = acc_xyz[1]
+    rosimu.linear_acceleration.z = acc_xyz[2]
+    
+    bag.write('/imu0', rosimu, rospy.Time(timestamp))
+
+    count += 1
+    progBar.update(count)
+
+imu_file.close()
+
+
+# Convert the video
+bridge = cv_bridge.CvBridge()
+cap = cv2.VideoCapture(video_fname)
+total_num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+frame_rate = cap.get(cv2.CAP_PROP_FPS)
+
+frame_time = time_offset
+flag, frame = cap.read()
+
+print("Processing video...")
+progBar = progressbar.ProgressBar(max_value=total_num_frames)
+frame_count = 0
+while (flag):
+    height, width = frame.shape[:2]
+    frame = cv2.resize(frame,(int(width*scale), int(height*scale)), interpolation = cv2.INTER_AREA)
+    msg = bridge.cv2_to_imgmsg(frame, "passthrough")
+    ros_time = rospy.Time(frame_time)
+    msg.header.stamp = ros_time
+
+    bag.write('/cam0/image_raw/', msg, ros_time)
+
+    frame_time += 1/frame_rate
+    flag, frame = cap.read()
+    frame_count += 1
+    progBar.update(frame_count)
+
+cap.release()
+
+bag.close()
+bag_file.close()
