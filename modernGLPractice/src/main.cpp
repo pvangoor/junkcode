635412a0160ad7bbe571ef65b46383f2730865c9
Pieter van Goor <pietervangoor@anu.edu.au>
2019-09-12 21:57:03 +1000

    Simplify code and fill with comments

--- modernGLPractice/src/main.cpp
+++ modernGLPractice/src/main.cpp
@@ -46,37 +46,42 @@ int main(int argc, char** argv) {
 	-0.5f, -0.5f,  0.0f
 	};
 
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+	GLuint vbo = 0; // vertex buffer object
+	glGenBuffers(1, &vbo); // assigns a buffer name to vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); // bind vbo to the GL_ARRAY_BUFFER
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW); // initialise the data store of vbo with points.
+	// The final argument makes a promise about how data will be handled in the buffer.
+	// STATIC means 'modified once and used many times'
 
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	GLuint vao = 0; //vertex array object
+	glGenVertexArrays(1, &vao); // assigns an array name to vao
+	glBindVertexArray(vao); // binds vao with the name vao
+	glEnableVertexAttribArray(0); // enables vertex attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
-	glCompileShader(vs);
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
-	glCompileShader(fs);
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER); // create a vertex shader
+	glShaderSource(vs, 1, &vertex_shader, NULL); // attach the vertex shader source
+	glCompileShader(vs); // compile the vertex shader
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER); // create a fragment shader
+	glShaderSource(fs, 1, &fragment_shader, NULL); // attach the fragment shader source
+	glCompileShader(fs); // compile the fragment shader
 
+	// Create a shader program
 	GLuint shader_programme = glCreateProgram();
 	glAttachShader(shader_programme, fs);
 	glAttachShader(shader_programme, vs);
 	glLinkProgram(shader_programme);
+	glDeleteShader(vs);
+	glDeleteShader(fs);  
 
 	while(!glfwWindowShouldClose(window)) {
 		// wipe the drawing surface clear
+		glClearColor(0.75f, 0.75f, 0.75f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(shader_programme);
 		glBindVertexArray(vao);
 		// draw points 0-3 from the currently bound VAO with current in-use shader
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_LINE_STRIP, 0, 3);
 		// update other events like input handling 
 		glfwPollEvents();
 		// put the stuff we've been drawing onto the display
