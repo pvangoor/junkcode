1f84cb0b43705b9632ea5a0bd03e957632e0a709
Pieter van Goor <pietervangoor@anu.edu.au>
2020-05-14 10:29:26 +1000

    Add video2folder converter

---
+++ converters/video2folder.py
@@ -0,0 +1,41 @@
+import argparse
+import cv2
+import os
+import progressbar
+
+parser = argparse.ArgumentParser(description="Convert a video file to a folder of images.")
+parser.add_argument('video', metavar='v', type=str, help="The file name of the video to convert")
+args = parser.parse_args()
+
+
+videoFname = args.video
+folderName = videoFname[:-4]
+if not os.path.exists(folderName):
+    os.makedirs(folderName)
+
+cap = cv2.VideoCapture(videoFname)
+flag, frame = cap.read()
+
+# frame_rate = cap.get(cv2.CAP_PROP_FPS)
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+# time = 1/frame_rate
+# last_time = 0
+
+digits = str(len(str(frame_count)))
+count = 0
+progBar = progressbar.ProgressBar(max_value=frame_count)
+while (flag):
+    imgFname = ("{:0>"+str(digits)+"d}.png").format(count)
+    imgFname = folderName+"/"+imgFname
+
+    cv2.imwrite(imgFname, frame)
+
+    count += 1
+    progBar.update(count)
+
+
+    flag, frame = cap.read()
+
+
+
+
