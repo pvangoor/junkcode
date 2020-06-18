a834419f23a586f11c5d72363fa0f6d764f99619
Pieter van Goor <pietervangoor@anu.edu.au>
2020-06-19 08:56:51 +1000

    Fix latex comment deletion

--- utility/destroy_latex_comments.py
+++ utility/destroy_latex_comments.py
@@ -4,10 +4,15 @@ parser = argparse.ArgumentParser(description="Delete all comments from a latex f
 parser.add_argument('fname', metavar='f', type=str, help="The file name of the latex file.")
 args = parser.parse_args()
 
+comment_string = "%%%%%%%%%% - comment removed - %%%%%%%%%%"
+
 latexFname = args.fname
 with open(latexFname, 'r') as fin, open("nocomment_"+latexFname, 'w') as fout:
     for line in fin:
         i = line.find("%")
-        if i >= 0 and (i == 0 or line[i-1] != "\\"):
-            line = line[:i]+'\n'
+        if i >= 0:
+            # A comment is found!
+            if (i == 0) or line[i-1] != "\\":
+                # An inline comment. Just kill these.
+                line = line[:i]+comment_string+'\n'
         fout.write(line)
