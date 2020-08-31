34461c46a93695d9834becea7cef7d8a19b85b5d
Pieter van Goor <pieter.vangoor@anu.edu.au>
2020-08-31 11:40:56 +1000

    Add script for showing an animated trajectory

---
+++ utility/animate_trajectory.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from pylie import plotting
+from pylie import SE3
+import argparse
+import csv
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+parser = argparse.ArgumentParser(description="Animate the trajectory of a pose in a file.")
+parser.add_argument("file", metavar='f', type=str, help="Name fo the file with the poses.")
+parser.add_argument("--fspec", type=str, default='xw', help="Format of the poses in the file. Default xw.")
+parser.add_argument("--scol", type=int, default=1, help="The column where the poses start. Default 1.")
+parser.add_argument("--srow", type=int, default=1, help="The row where the poses start. Default 1.")
+
+args = parser.parse_args()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
+with open(args.file, 'r') as f:
+    reader = csv.reader(f)
+
+    # Ignore the header
+    for _ in range(args.srow):
+        next(reader)
+
+    # Read the poses and animate trajectory
+    trail = np.zeros((3,0))
+    for line in reader:
+        pose = SE3.from_list(line[args.scol:], args.fspec)
+        trail = np.hstack((trail,pose.x()))
+        # print(pose.q())
+
+        ax.cla()
+
+        ax.plot(trail[0,:], trail[1,:], trail[2,:], "m")
+        plotting.plotFrame(pose, ax)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        ax.set_box_aspect(np.max(trail, axis=1))
+
+        plt.pause(0.02) # 50 Hz
+
