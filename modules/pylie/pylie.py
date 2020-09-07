f49788493ea1a67233ff213012f77f16207a735e
Pieter van Goor <pieter.vangoor@anu.edu.au>
2020-09-07 13:55:51 +1000

    Add SOT3 to import target

--- modules/pylie/pylie.py
+++ modules/pylie/pylie.py
@@ -1,5 +1,6 @@
 from LieGroup import LieGroup as LieGroup
 from SO3 import SO3 as SO3
+from SOT3 import SOT3 as SOT3
 from SE3 import SE3 as SE3
 from SIM3 import SIM3 as SIM3
 from S1 import S1 as S1
