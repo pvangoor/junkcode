b5faafab1498dde97531a771bf183c4d20253350
Pieter van Goor <pieter.vangoor@anu.edu.au>
2021-07-28 11:02:46 +1000

    Fix memory issues with double mult and add

--- practice/expressions/expressions.cpp
+++ practice/expressions/expressions.cpp
@@ -12,16 +12,8 @@ struct NumExpression {
    double derivative() const {return static_cast<E const&>(*this).derivative();}
 };
 template<typename E>
+// concept num_expr = is_base_of<NumExpression<E>,E>::value;
 concept num_expr = disjunction<is_base_of<NumExpression<E>,E>, is_floating_point<E>>::value;
-
-template<typename E>
-struct VarExpression {
-   double operator()(const double& x) const { return value(x); }
-   double value(const double& x) const { return static_cast<E const&>(*this).value(x); }
-   double derivative(const double& x) const {return static_cast<E const&>(*this).derivative(x);}
-};
-template<typename E>
-concept var_expr = is_base_of_v<VarExpression<E>,E>;
 
 template<num_expr E>
 double get_value(const E& u) {return u.value(); }
@@ -30,17 +22,10 @@ double get_derivative(const E& u) {return u.derivative(); }
 double get_value(const double& u) {return u; }
 double get_derivative(const double& u) {return 0.0; }
 
-template<var_expr E>
-double get_value(const E& u, const double& x) {return u.value(x); }
-template<var_expr E>
-double get_derivative(const E& u, const double& x) {return u.derivative(x); }
-double get_value(const double& u, const double& x) {return u; }
-double get_derivative(const double& u, const double& x) {return 0.0; }
-
 struct Par : NumExpression<Par> {
     double val;
 
-    Par(const double& v) : val(v) {};
+    Par(const double v) : val(v) {};
     double value() const {return val;}
     double derivative() const {return 1.0;}
 
@@ -48,10 +33,10 @@ struct Par : NumExpression<Par> {
     Par(NumExpression<E> const& expr) : val(expr.value()) {}
 };
 
-struct Var : VarExpression<Var> {
-   double value(const double& x) const {return x;}
-   double derivative(const double& x) const {return 1.0;}
-};
+// struct Var : VarExpression<Var> {
+//    double value(const double& x) const {return x;}
+//    double derivative(const double& x) const {return 1.0;}
+// };
 
 // Addition
 template <num_expr E1, num_expr E2>
@@ -60,6 +45,26 @@ struct NumSum : public NumExpression<NumSum<E1, E2> > {
     E2 const& _v;
 
     NumSum(E1 const& u, E2 const& v) : _u(u), _v(v) {}
+    double value() const { return get_value(_u) + get_value(_v); }
+    double derivative() const { return get_derivative(_u) + get_derivative(_v); }
+};
+
+template <num_expr E1>
+struct NumSum<E1, double> : public NumExpression<NumSum<E1, double> > {
+    E1 const& _u;
+    double const _v;
+
+    NumSum(E1 const& u, double const& v) : _u(u), _v(v) {}
+    double value() const { return get_value(_u) + get_value(_v); }
+    double derivative() const { return get_derivative(_u) + get_derivative(_v); }
+};
+
+template <num_expr E2>
+struct NumSum<double, E2> : public NumExpression<NumSum<double, E2> > {
+    double const _u;
+    E2 const& _v;
+
+    NumSum(double const& u, E2 const& v) : _u(u), _v(v) {}
     double value() const { return get_value(_u) + get_value(_v); }
     double derivative() const { return get_derivative(_u) + get_derivative(_v); }
 };
@@ -82,35 +87,6 @@ operator+(double const& u, NumExpression<E2> const& v) {
    return NumSum<double, E2>(u, *static_cast<const E2*>(&v));
 };
 
-// Variable Addition
-template <typename E1, typename E2>
-struct VarSum : public VarExpression<VarSum<E1, E2> > {
-    E1 const& _u;
-    E2 const& _v;
-
-    VarSum(E1 const& u, E2 const& v) : _u(u), _v(v) {}
-    double value(const double& x) const { return get_value(_u, x) + get_value(_v, x); }
-    double derivative(const double& x) const { return get_derivative(_u, x) + get_derivative(_v, x); }
-};
-
-template <typename E1, typename E2>
-VarSum<E1, E2>
-operator+(VarExpression<E1> const& u, VarExpression<E2> const& v) {
-   return VarSum<E1, E2>(*static_cast<const E1*>(&u), *static_cast<const E2*>(&v));
-};
-
-template <typename E1>
-VarSum<E1, double>
-operator+(VarExpression<E1> const& u, double const& v) {
-   return VarSum<E1, double>(*static_cast<const E1*>(&u), v);
-};
-
-template <typename E2>
-VarSum<double, E2>
-operator+(double const& u, VarExpression<E2> const& v) {
-   return VarSum<double, E2>(u, *static_cast<const E2*>(&v));
-};
-
 // Multiplication
 template <typename E1, typename E2>
 struct NumProd : public NumExpression<NumProd<E1, E2> > {
@@ -118,6 +94,26 @@ struct NumProd : public NumExpression<NumProd<E1, E2> > {
     E2 const& _v;
 
     NumProd(E1 const& u, E2 const& v) : _u(u), _v(v) {}
+    double value() const { return get_value(_u) * get_value(_v); }
+    double derivative() const { return get_value(_v) * get_derivative(_u) + get_value(_u) * get_derivative(_v); }
+};
+
+template <typename E2>
+struct NumProd<double, E2> : public NumExpression<NumProd<double, E2> > {
+    double const _u;
+    E2 const& _v;
+
+    NumProd(double const& u, E2 const& v) : _u(u), _v(v) {}
+    double value() const { return get_value(_u) * get_value(_v); }
+    double derivative() const { return get_value(_v) * get_derivative(_u) + get_value(_u) * get_derivative(_v); }
+};
+
+template <typename E1>
+struct NumProd<E1, double> : public NumExpression<NumProd<E1, double> > {
+    E1 const& _u;
+    double const _v;
+
+    NumProd(E1 const& u, double const& v) : _u(u), _v(v) {}
     double value() const { return get_value(_u) * get_value(_v); }
     double derivative() const { return get_value(_v) * get_derivative(_u) + get_value(_u) * get_derivative(_v); }
 };
@@ -172,6 +168,12 @@ log(NumExpression<E> const& u) {
    return NumLog<E>(*static_cast<const E*>(&u));
 };
 
+template<typename F>
+auto Derivative(const F & f) {
+   auto Df = [&f](const auto& x) { return get_derivative(f(x));};
+   return Df;
+}
+
 
 int main() {
    // Write C++ code here
@@ -185,11 +187,26 @@ int main() {
    cout << sum3.derivative() << endl;
 
 
-   Var z;
-   auto f = z + 5.0;
+   // Var z;
+   auto f = [](const Par& z) {
+      auto y = 5.0 * z;
+      return y;
+      // auto y2 = 2.0 + y;
+      // return exp(y2);
+   };
+   auto df = Derivative(f);
 
-   cout << z(3.0) << endl;
-   cout << f(4.0) << endl;
+   // cout << z(3.0) << endl;
+   Par z = 4.0;
+   auto fz = f(z);
+   // auto fz = 5.0 + z;
+   auto fz2 = 2.0 + fz;
+   double y = get_value(fz2);
+
+   double dfdz = get_value(df(z));
+
+   cout << y << endl;
+   cout << dfdz << endl;
 
 
 
