a0d713a11d6f80eb26e09af9d96c78e2f8248823
Pieter van Goor <pietervangoor@anu.edu.au>
2021-12-10 12:19:10 +1100

    Adjust SOT3 visualisation

--- visualisation/sot3_geometry.py
+++ visualisation/sot3_geometry.py
@@ -3,18 +3,19 @@ import numpy as np
 from pylie import SOT3
 
 from matplotlib.animation import FuncAnimation
+plt.rcParams['savefig.bbox'] = 'tight' 
 
-eta0 = np.reshape((0,0,1), (3,1))
+eta0 = np.reshape((1,0,0), (3,1))
 
-n_angles = 20
-n_scales = 8
-max_angle = np.pi / 4.0
+n_angles = 12
+n_scales = 6
+max_angle = np.pi / 3.0
 max_scale = 1.0
 
 omega1, omega2, s = np.meshgrid(np.linspace(-max_angle,max_angle,n_angles), np.linspace(-max_angle,max_angle,n_angles), np.linspace(-max_scale,max_scale,n_scales))
 
 def sot3normal(omega1, omega2, s):
-    x = SOT3.exp(np.reshape([omega1,omega2,0,s], (4,1))) * eta0
+    x = SOT3.exp(np.reshape([0,omega1,omega2,s], (4,1))) * eta0
     return x.ravel()
 
 sot3normal_vec = np.vectorize(sot3normal, signature="(),(),()->(3)")
@@ -34,11 +35,12 @@ for i in range(result.shape[0]):
         ax.plot(result[i,:,j,0], result[i,:,j,1], result[i,:,j,2], 'b-')
 # for i in range(result.shape[1]):
 #     ax.plot(result[:,i,0], result[:,i,1], result[:,i,2], 'b-')
+ax.set_title("Polar Geometry")
 
 # ax.plot(result[:,:,0], result[:,:,1], result[:,:,2], 'r-')
 # ax.plot(result[:,:,0].T, result[:,:,1].T, result[:,:,2].T, 'r-')
 # plt.show()
-
+ax.set_box_aspect(aspect = (1,1,1))
 def update(angle):
     ax.view_init(30, angle)
     return []
@@ -49,4 +51,4 @@ ani = FuncAnimation(fig, update, frames=np.linspace(0, 360, int(rev_time*hz)), b
 ani.save("sot3_geometry.mp4")
 # ani.save("sot3_geometry.gif")
 
-# plt.show()
+plt.show()
