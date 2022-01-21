c434b07293c63106d9f21f49092c5ff13f86ea18
Pieter van Goor <pietervangoor@anu.edu.au>
2022-01-21 15:12:14 +1100

    Constexpr where possible

--- practice/duals/hyperduals.cpp
+++ practice/duals/hyperduals.cpp
@@ -13,17 +13,17 @@ struct HyperDual
    HyperDual<N-1, Scalar> e; // The additional dual part
 
    HyperDual() = default;
-   HyperDual(const HyperDual<N-1, Scalar> &x, const HyperDual<N-1, Scalar> &e = 0) : x(x), e(e){};
+   constexpr HyperDual(const HyperDual<N-1, Scalar> &x, const HyperDual<N-1, Scalar> &e = 0) : x(x), e(e){};
    // template<typename S2>
    // HyperDual(const S2 &x) : x(HyperDual<N-1,Scalar>(x)), e(HyperDual<N-1, Scalar>(0.0)){};
    template<typename S2, typename S3 = Scalar>
-   HyperDual(const S2 &x, const S3 &e = 0.0) : x(HyperDual<N-1,Scalar>(x)), e(HyperDual<N-1, Scalar>(e)){};
+   constexpr HyperDual(const S2 &x, const S3 &e = 0.0) : x(HyperDual<N-1,Scalar>(x)), e(HyperDual<N-1, Scalar>(e)){};
 
    template<typename T>
-   static HyperDual Deriver(const T& x) {
+   constexpr static HyperDual Deriver(const T& x) {
       return HyperDual(HyperDual<N-1,Scalar>::Deriver(x), HyperDual<N-1,Scalar>(1.0));
    }
-   Scalar smallestDual() const {
+  constexpr Scalar smallestDual() const {
       if constexpr (N > 1) {
          return this->e.smallestDual();
       } else {
@@ -31,11 +31,11 @@ struct HyperDual
       }
    }
    
-   HyperDual operator*(const HyperDual &other) const
+   constexpr HyperDual operator*(const HyperDual &other) const
    {
       return HyperDual(this->x * other.x, this->x * other.e + this->e * other.x);
    }
-   HyperDual operator+(const HyperDual &other) const
+   constexpr HyperDual operator+(const HyperDual &other) const
    {
       return HyperDual(this->x + other.x, this->e + other.e);
    }
@@ -54,18 +54,18 @@ struct HyperDual<0, Scalar> {
 
    HyperDual() = default;
    template<typename T>
-   HyperDual(const T&x)
+   constexpr HyperDual(const T&x)
    requires is_convertible_v<T, Scalar>
     : x(x) {};
    template<typename S2, typename S3>
    HyperDual(const S2 &x) : x(x){};
 
    template<typename T>
-   static HyperDual Deriver(const T& x) {
+   constexpr static HyperDual Deriver(const T& x) {
       return HyperDual(x);
    }
 
-   operator Scalar() const { return this->x; }
+   constexpr operator Scalar() const { return this->x; }
    
    // template<typename T>
    // HyperDual operator*(const T &other) const
@@ -73,43 +73,48 @@ struct HyperDual<0, Scalar> {
    // {
    //    return HyperDual(this->x * HyperDual(other).x);
    // }
-   HyperDual operator*(const HyperDual &other) const
+   constexpr HyperDual operator*(const HyperDual &other) const
    {
       return HyperDual(this->x * other.x);
    }
 
-   HyperDual operator+(const HyperDual &other) const
+   constexpr HyperDual operator+(const HyperDual &other) const
    {
       return HyperDual(this->x + other.x);
    }
 };
 
 
+
 struct my_functor{
    template<typename T> 
-   T operator() (const T& x) const {
+   constexpr static T eval(const T& x) {
       // T y = exp(x);
       T z = x * x + x * 2 + 3;
       return z;
    }
 };
 
-template<size_t N, typename Scalar>
-Scalar derivative(const auto& f, const Scalar& x) {
+template<size_t N, typename Functor, typename Scalar>
+constexpr Scalar derivative(const Scalar& x) {
    // Returns the Nth derivative of f at x
    HyperDual<N> dnx = HyperDual<N>::Deriver(x);
-   return f(dnx).smallestDual();
+   return Functor::eval(dnx).smallestDual();
 }
 
 
 int main()
 {
-    double d3fdx = derivative<3>(my_functor{}, 1.0);
+      double x = 5.0;
+    constexpr double d3fdx = derivative<3, my_functor>(1.0);
+    double d2fdx = derivative<3, my_functor>(x);
 
-   cout << derivative<1>(my_functor{}, 3.0) << endl;
-   cout << derivative<2>(my_functor{}, 3.0) << endl;
-   cout << derivative<3>(my_functor{}, 3.0) << endl;
+   cout << derivative<1, my_functor>(3.0) << endl;
+   cout << derivative<2, my_functor>(3.0) << endl;
+   cout << derivative<3, my_functor>(3.0) << endl;
 
+   // cout << derivative<1, my_functor, double>(3.0) << endl;
+   // cout << evaluate<my_functor, double>(1.0) << endl;
 
 
     return 0;
